cb3e942e40a16d327616c501e8b062630dcba72d
Satish Babariya <satish.babariya@gmail.com>
2021-12-19 11:27:42 +0530

    add protogen for swift

--- protogen.sh
+++ protogen.sh
@@ -13,3 +13,14 @@ fi
 protoc --proto_path=proto --go_out=mongorpc/v1 --go_opt=paths=source_relative \
     --go-grpc_out=mongorpc/v1 --go-grpc_opt=paths=source_relative \
     proto/*.proto
+
+
+# if mongorpc-swift/proto dir is not exist, create it
+if [ ! -d "mongorpc-swift/proto" ]; then
+    mkdir mongorpc-swift/proto
+fi
+
+# protogen swift
+protoc -I=proto --proto_path=proto --swift_out=mongorpc-swift/proto \
+    --grpc-swift_out=Client=true,Server=false:mongorpc-swift/proto \
+    proto/*.proto
